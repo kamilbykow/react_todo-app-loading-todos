0ea946db46f2be20dc20e42805ea536ad75e432e
Kamil Bykowski <kamilbykowski99@gmail.com>
2025-08-06 16:10:02 +0200

    Solution

---
+++ src/components/ErrorNotification/ErrorNotification.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+type Props = {
+  hidden: boolean;
+};
+
+export const ErrorNotification: React.FC<Props> = ({ hidden }) => {
+  const [hide, setHide] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setHide(false);
+    }, 3000);
+  }, [hidden]);
+
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className={`notification is-danger is-light has-text-weight-normal ${hide ? '' : 'hidden'}`}
+    >
+      <button data-cy="HideErrorButton" type="button" className="delete" />
+      {/* show only one message at a time */}
+      Unable to load todos
+      {/* <br />
+      Title should not be empty
+      <br />
+      Unable to add a todo
+      <br />
+      Unable to delete a todo
+      <br />
+      Unable to update a todo */}
+    </div>
+  );
+};
